42f64803d95dda389ccac57315d27f0aafacc3aa
mikhael.lopez <lopez.mikhael@gmail.com>
2022-08-11 12:15:46 +0200

    [SUPPORT] Update deps

--- presentation/src/main/java/com/mikhaellopez/presentation/di/modules/ApplicationModule.kt
+++ presentation/src/main/java/com/mikhaellopez/presentation/di/modules/ApplicationModule.kt
@@ -2,8 +2,6 @@ package com.mikhaellopez.presentation.di.modules
 
 import android.app.Application
 import android.content.Context
-import android.content.SharedPreferences
-import androidx.preference.PreferenceManager
 import com.mikhaellopez.presentation.di.PerApplication
 import dagger.Module
 import dagger.Provides
@@ -19,10 +17,5 @@ class ApplicationModule {
     @Provides
     @PerApplication
     internal fun provideContext(application: Application): Context = application.baseContext
-
-    @Provides
-    @PerApplication
-    internal fun providesSharedPreferences(context: Context): SharedPreferences =
-        PreferenceManager.getDefaultSharedPreferences(context)
 
 }
